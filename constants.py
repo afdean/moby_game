dd5a1ed40e50c48dd7f8dd3f35f9e3176c8b5f54
patrickwest <patrickwest@berkeley.edu>
2017-01-29 00:31:27 -0800

    Added basic game world construction constants

--- constants.py
+++ constants.py
@@ -1,3 +1,12 @@
 """
 File containing constants to be used throughout the game
 """
+
+#General constants
+SCREEN = [1024, 768]
+WORLD = [1024, 768]
+TICK = 60
+
+#Miscellaneous constants
+INFINITY = float("inf")
+EPSILON = 0.000001
