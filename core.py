02ef3e59b42dd4b5348142792588b63fd7166dc0
patrickwest <patrickwest@berkeley.edu>
2017-01-30 23:03:18 -0800

    Added the GameWorld class and created run.py
    run.py is what is executed to run the game. At this moment it should briefly open a window and then quit. Also added import statements to the beginning of core.py and run.py

--- core.py
+++ core.py
@@ -1,3 +1,41 @@
 """
 File containing main functions to run the game
 """
+
+import sys
+import math
+import random
+import time
+import pygame
+import numpy
+from pygame.locals import *
+
+class GameWorld():
+
+    """
+    Overarching structure holding the screen, actors, objects, etc.
+    """
+
+    def __init__(self, seed, worlddimensions, screendimensions):
+        #initialize random seed
+        self.time = time.time()
+        random.seed(self.time)
+        #initialize pygame and set up screen and background surface
+        pygame.init()
+        screen = pygame.display.set_mode(screendimensions)
+        # Background surface that will hold everything
+        background = pygame.Surface(worlddimensions)
+        background = background.convert()
+        background.fill((255, 255, 255))
+        # Debug surface
+        debug = pygame.Surface(worlddimensions)
+        debug = debug.convert()
+        debug.fill((255, 255, 255))
+        background.blit(debug, (0, 0))
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
+        #store stuff
+        self.screen = screen
+        self.seed = seed or self.time
+        self.background = background
+        self.debug = debug
---
+++ run.py
@@ -0,0 +1,16 @@
+"""
+File where the game is executed
+"""
+
+import sys
+import math
+import random
+import time
+import pygame
+import numpy
+from pygame.locals import *
+
+from constants import *
+from core import *
+
+world = GameWorld(None, (1224, 900), (1224, 900))
