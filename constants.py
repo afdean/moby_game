62b898ed2d5da2489a6fd2fba7c1d954899c9a8f
AFDean00 <afdean00@gmail.com>
2017-02-08 21:56:29 -0500

    feat: Add colors to constants

--- constants.py
+++ constants.py
@@ -10,3 +10,11 @@ TICK_RATE = 60
 #Miscellaneous constants
 INFINITY = float("inf")
 EPSILON = 0.000001
+
+#Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+PURPLE = (150, 0, 150)
